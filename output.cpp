81196b850b2ba5d47d311a945e14e7baa923258f
sux <gabriel1861@gmail.com>
2022-06-30 15:39:42 +0800

    second version

---
+++ output.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "output.h"
+
+void output(){
+    std::cout << "This is the multifile" << std::endl;
+}
--- test.cpp
+++ test.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "output.h"
 
 int main(){
+    output();
     std::cout << "Hello World" << std::endl;
     return 0;
 }
