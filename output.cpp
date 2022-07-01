97524d1da0b07a6bce71658dc9edf854dcf8f1a8
sux <gabriel1861@gmail.com>
2022-07-01 15:46:25 +0800

    add for aliyun

--- output.cpp
+++ output.cpp
@@ -4,4 +4,5 @@
 void output(){
     std::cout << "This is the multifile" << std::endl;
     std::cout << "This is another modified" << std::endl;
+    std::cout << "This is only for aliyun" << std::endl;
 }
